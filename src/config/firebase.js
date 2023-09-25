2af885f36a4ce7ba867bcdfba31fa0bec3603ecb
raissalab <125493288+raissalab@users.noreply.github.com>
2023-09-25 08:15:54 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,12 @@ import { initializeAuth } from 'firebase/auth';
 
 //Aqui vocês devem colocar as suas credenciais
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+ apiKey: "AIzaSyCsyoGWEPQ9XS5mM_28p5wLxYybmQWzTL4",
+  authDomain: "raissaifal513.firebaseapp.com",
+  projectId: "raissaifal513",
+  storageBucket: "raissaifal513.appspot.com",
+  messagingSenderId: "1050270803151",
+  appId: "1:1050270803151:web:b60afaa21b3b94fc07c3ba"
 };
 
 const app = initializeApp(firebaseConfig);
